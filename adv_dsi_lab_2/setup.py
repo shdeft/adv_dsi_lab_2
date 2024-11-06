9b2d1e0f81200be883138dc155c412b47a48c79f
Shadi Eftekhari <shadieftekhari@Shadis-MacBook-Air.local>
2024-11-06 12:24:17 +1100

    init

---
+++ adv_dsi_lab_2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='UTS Assignment',
+    author='Shadi Eftekhari',
+    license='MIT',
+)
